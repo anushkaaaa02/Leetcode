033a68560691a21c6968a106c950c62a92a9fd4f
anushkaaa02 <anushkanaman5@gmail.com>
2026-06-15 15:49:09 +0530

    Add 0509 Fibbonaci number

---
+++ 0509-Fibbonaci-number/0509-fibbonaci-number.java
@@ -0,0 +1,24 @@
+class Solution{
+    public int fib(int n){
+        int a = 0,
+        int b = 1,
+        int c = 0;
+        if(n==0){
+            return a;
+        }
+        if(n==1){
+            return b;
+        }
+        int (n==2){
+            return c;
+        }
+        int count = 3;
+        while(count<=n){
+            c = b + d;
+           d = b;
+           b = c; 
+           count ++;
+        }
+        return d;
+    }
+}
