de7716c7c22ff4c9079881573a07e11a039e7cfd
anushkaaa02 <anushkanaman5@gmail.com>
2026-06-15 15:23:20 +0530

    Add 2420 count the digits that divide a number

---
+++ 2520-Count-the-digit-that-divide-a-number/2420-count-the-digit-that-divide-the-number.java
@@ -0,0 +1,14 @@
+public class 2420-Solution{
+    int countDigits(int nums){
+        int count = 0;
+        int original = nums;
+        while(nums>0){
+            int digit = nums % 10;
+            if(digit !=0 && original % digit == 0){
+                count++;
+            }
+            nums = nums / 10;
+        }
+        return count;
+    }
+}
