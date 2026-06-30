9b9030cc219a58fbea00f6890bed8256c6e97e1f
anushkaaa02 <anushkanaman5@gmail.com>
2026-06-30 22:52:08 +0530

    Add sum of unique elements

---
+++ 1748-Sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,15 @@
+class Solution{
+    public int sumofUnique(int[] nums){
+        int[] freq = new int[101];
+        int sum = 0;
+        for(int num : nums){
+            freq[num]++;
+        }
+        for(int i = 0; i < freq.length; i++){
+            if(freq[i] == 1){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
