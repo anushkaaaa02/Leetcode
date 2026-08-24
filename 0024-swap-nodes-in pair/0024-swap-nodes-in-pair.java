07da8658c2f0ba9d56d1b6d218197cc5b1a6aff9
anushkaaa02 <anushkanaman5@gmail.com>
2026-08-24 22:45:49 +0530

    Add prime arrangements

---
+++ 0024-swap-nodes-in pair/0024-swap-nodes-in-pair.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode first = head;
+        ListNode second = head.next;
+
+        // Swap first pair
+        first.next = swapPairs(second.next);
+        second.next = first;
+
+        return second;
+    }
+}
