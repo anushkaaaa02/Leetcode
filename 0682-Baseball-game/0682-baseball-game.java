803190c84a905e59a9496cd652f051308517ecfb
anushkaaa02 <anushkanaman5@gmail.com>
2026-09-03 23:00:09 +0530

    Add baseball game

---
+++ 0682-Baseball-game/0682-baseball-game.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> st = new Stack<>();
+
+        for (String op : operations) {
+
+            if (op.equals("+")) {
+                int top = st.pop();
+                int second = st.peek();
+
+                st.push(top);
+                st.push(top + second);
+
+            } else if (op.equals("D")) {
+                st.push(2 * st.peek());
+
+            } else if (op.equals("C")) {
+                st.pop();
+
+            } else {
+                st.push(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+
+        while (!st.isEmpty()) {
+            sum += st.pop();
+        }
+
+        return sum;
+    }
+}
